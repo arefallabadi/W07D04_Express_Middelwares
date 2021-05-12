aff56be6533e527963df6571265aa64200293a75
arefallabadi <labadi.aref@gmail.com>
2021-05-12 14:13:15 +0300

    practice Q1 && Q2

--- index.js
+++ index.js
@@ -7,59 +7,61 @@ const port = 3000;
 
 const users = ["John", "Mark"];
 
-
-app.use((req,res,next)=>{
-    if(users.length === 0){
-        next("err")
-    }
-    else{
-        next()
-    }
-})
+app.use((req, res, next) => {
+  if (users.length === 0) {
+    next("err");
+  } else {
+    next();
+  }
+});
 
 app.get("/users", (req, res, next) => {
   res.json(users);
 });
 
-//Q1
-const logUsers =(req,res,next)=>{
-console.log(users);
-next()
-}
+const logUsers = (req, res, next) => {
+  console.log(users);
+  next();
+};
 
-app.use(logUsers)
+app.use(logUsers);
 
-const logMethod = (req,res,next)=>{
-    console.log(req.method);
-}
+const logMethod = (req, res, next) => {
+  console.log(req.method);
+};
 
+app.use("/users", logMethod);
 
-app.use("/users",logMethod)
+app.use(express.json());
 
-
-app.use(express.json())
-
-app.use((err,req,res,next)=>{
-    console.log("No users");
-    res.json("No users")
-})
-
+app.use((err, req, res, next) => {
+  console.log("No users");
+  res.json("No users");
+});
 
 //Practice
 //Q1
 const auth = express.Router();
-const aa = (req,res,next)=>{
-    // res.json(users)
-    console.log("hi");  
-    next()
-}
-auth.get("/users",aa,(req,res)=>{
-res.json(users.forEach((e,i)=>{
-e
-}))
-})
+auth.get("/users", (req, res, next) => {
+  res.json(users);
+});
+
+// app.use("/auth", auth);
+
 
 //Q2
+auth.post("/users/create",(req,res,next)=>{
+   const name = req.body.name
+   users.push(name)
+res.json(users)
+})
+
+//Q3
+
+
+
+
+
 
 
 
