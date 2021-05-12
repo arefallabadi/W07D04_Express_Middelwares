bbab4f404e35a519703fb7fa5f1c1f2d5c10eb2d
arefallabadi <labadi.aref@gmail.com>
2021-05-12 12:50:56 +0300

    pulse done

--- index.js
+++ index.js
@@ -6,8 +6,16 @@ const app = express();
 const port = 3000;
 
 const users = ["John", "Mark"];
-app.use(express.json())
 
+
+app.use((req,res,next)=>{
+    if(users.length === 0){
+        next("err")
+    }
+    else{
+        next()
+    }
+})
 
 app.get("/users", (req, res, next) => {
   res.json(users);
@@ -19,7 +27,41 @@ console.log(users);
 next()
 }
 
+app.use(logUsers)
+
+const logMethod = (req,res,next)=>{
+    console.log(req.method);
+}
+
+
+app.use("/users",logMethod)
+
+
+app.use(express.json())
+
+app.use((err,req,res,next)=>{
+    console.log("No users");
+    res.json("No users")
+})
+
+
+//Practice
+//Q1
+const auth = express.Router();
+const aa = (req,res,next)=>{
+    // res.json(users)
+    console.log("hi");  
+    next()
+}
+auth.get("/users",aa,(req,res)=>{
+res.json(users.forEach((e,i)=>{
+e
+}))
+})
+
 //Q2
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
